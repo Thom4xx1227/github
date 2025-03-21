d25b15fbabb5aad5cc5b741bb442f6941ebe885e
Yulian4 <yulsyay19@gmail.com>
2025-03-21 13:24:05 -0500

    agrega funcion de saludar en main

---
+++ ejercicio1.js
@@ -0,0 +1,32 @@
+function main(nombre) {
+
+    console.log(`bienvenido al programa ${nombre}`)
+
+}
+
+function saludarEstudiante(nombre) {
+
+    console.log( `hola estudiante ${nombre}`)
+    
+}
+
+function saludarProfesor(nombre) {
+
+    console.log(`hola profesor ${nombre}`)
+    
+}
+
+// ejercicio 2
+
+function saludar (nom) {
+
+    console.log(`hola como estas ${nom}`)
+    
+}
+
+
+
+main ("Thom4x")
+saludarEstudiante ("thom4x")
+saludarProfesor ("german")
+saludar ("juan")
